869d9be86fd3c89f3057d4bf7af5baae8c0359da
Ris Ahamed <riswanahamed38@gmail.com>
2026-01-30 12:07:37 +0530

    Main.ts page updated and the angular core package installed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { inject } from '@vercel/analytics';
 
-bootstrapApplication(RootComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes)
